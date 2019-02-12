33db74656f862227ecadfe66a93125ce4e628c47
nszyc <nszyc@outlook.com>
2019-02-12 19:20:46 +0800

    Create 896_a.js

---
+++ 896_a.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function(A) {
+	return isMonotoneIncreasing(A) || isMonotoneDecreasing(A)
+};
+
+var isMonotoneIncreasing = function(array) {
+	if (array.length == 1) {
+		return true
+	}
+	for (var i = 0; i < array.length - 1; i++) {
+		if (array[i] > array[i + 1]) {
+			return false
+		}
+	}
+	return true
+}
+
+var isMonotoneDecreasing= function(array) {
+	if (array.length == 1) {
+		return true
+	}
+	for (var i = 0; i < array.length - 1; i++) {
+		if (array[i] < array[i + 1]) {
+			return false
+		}		
+	}
+	return true
+}
