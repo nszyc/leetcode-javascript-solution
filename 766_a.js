52148531f5145d60ae57f5e589e944d96d2db0b5
nszyc <nszyc@outlook.com>
2019-02-12 14:09:17 +0800

    Create 766_a.js

---
+++ 766_a.js
@@ -0,0 +1,10 @@
+var isToeplitzMatrix = function(matrix) {
+	for (var i = 1; i < matrix.length; i++) {
+		for (var j = 1; j < matrix[0].length; j++) {
+			if (matrix[i][j] != matrix[i - 1][j - 1]) {
+				return false
+			}
+		}
+	}
+	return true
+};
